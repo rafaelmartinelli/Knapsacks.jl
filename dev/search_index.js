97be2768a4d6eecc7bd39103c0429a1c26792294
Documenter.jl <documenter@juliadocs.github.io>
2021-05-05 18:49:05 +0000

    build based on a4901c7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KnapsackLib","category":"page"},{"location":"#KnapsackLib","page":"Home","title":"KnapsackLib","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KnapsackLib.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KnapsackLib]","category":"page"}]
+}
